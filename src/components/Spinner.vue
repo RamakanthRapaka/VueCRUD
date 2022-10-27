965d1888dc06e0e60bf77cd549d1261b5129e1f9
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-10-27 09:42:54 +0530

    first vueCRUD Commit

---
+++ src/components/Spinner.vue
@@ -0,0 +1,12 @@
+<template>
+<h2>Spinner</h2>
+</template>
+
+<script>
+export default{
+       name : 'Spinner'
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/ContactManager.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="container mt-3">
+       <div class="row">
+              <div class="col">
+                     <p class="h3 text-success fw-bold">
+                            Contact Manager
+                     <router-link to="/contacts/add" class="btn btn-success btn-sm"><i class="fa fa-plus-circle"></i> New</router-link>
+                     </p>
+                     <p class="fst-italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus totam molestias ipsa blanditiis voluptatibus adipisci omnis optio, asperiores, ab ipsam dicta dolor aspernatur ea. Sequi expedita iure saepe tenetur enim.</p>
+                     <form>
+                            <div class="row">
+                                   <div class="col-md-6">
+                                          <div class="row">
+                                          <div class="col">
+                                   <input type="text" class="form-control" placeholder="Search Name">
+                            </div>
+                            <div class="col">
+                                   <input type="submit" class="btn btn-outline-dark">
+                            </div>
+                            </div>
+                                   </div>
+                            </div>
+                     </form>
+              </div>
+       </div>
+</div>
+</template>
+
+<script>
+export default{
+       name : 'ContactManager'
+}
+</script>
+
+<style scoped>
+</style>
