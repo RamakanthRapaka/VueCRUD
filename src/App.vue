c305120628868fffbade087591f0af005e71d2f9
ramakanthrapaka-technobrain <ramakanth.rapaka@technobraingroup.com>
2022-10-28 15:57:00 +0530

    spinner added

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
+       <SpinNer></SpinNer>
        <nav-bar></nav-bar>
        <router-view></router-view>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import SpinNer from './components/Spinner.vue'
 export default{
-  components: { NavBar },
+  components: { NavBar, SpinNer },
        name : 'App'
 }
 </script>
